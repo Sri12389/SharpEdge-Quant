577b48d2939a8f167bf8e30eb2d55792af186097
Srijan E <Srijanreddye@gmail.com>
2025-06-03 10:34:00 -0400

    Improve Netlify deployment and UI/UX

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
+import LoadingSpinner from './components/LoadingSpinner.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary fallback={<div>Something went wrong.</div>}>
+      <Suspense fallback={<LoadingSpinner />}>
+        <App />
+      </Suspense>
+    </ErrorBoundary>
+  </React.StrictMode>
 );
